46bef1cf757c1b47ffa7910f0c00b995a0cab298
Scofield088 <oforu81@gmail.com>
2025-08-14 21:36:38 +0530

    updated dashboard.tsx

---
+++ frontend/src/Pages/dashboard.tsx
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react"
+import { Button } from "../components/Button"
+import { Card } from "../components/Card"
+import { CreateContentModal } from "../components/CreateContentModal"
+import { PlusIcon } from "../icons/PlusIcon"
+import { ShareIcon } from "../icons/ShareIcon"
+import { Sidebar } from "../components/Sidebar"
+import { useContent } from "../hooks/useContent"
+import { BACKEND_URL } from "../config"
+import axios from "axios"
+
+export function Dashboard() {
+  const [modalOpen, setModalOpen] = useState(false);
+  const {content, refresh} = useContent();
+
+  useEffect(() => {
+    refresh();
+  }, [modalOpen])
+
+  return <div>
+    <Sidebar />
+    <div className="p-4 ml-72 min-h-screen bg-gray-100 border-2">
+      <CreateContentModal open={modalOpen} onClose={() => {
+        setModalOpen(false);
+      }} />
+      <div className="flex justify-end gap-4">
+        <Button onClick={() => {
+          setModalOpen(true)
+        }} variant="primary" text="Add content" startIcon={<PlusIcon />}></Button>
+        <Button onClick={async () => {
+            const response = await axios.post(`${BACKEND_URL}/api/v1/brain/share`, {
+                share: true
+            }, {
+                headers: {
+                    "token": localStorage.getItem("token")
+                }
+            });
+            const shareUrl = `http://localhost:5173/share/${response.data.hash}`;
+            alert(shareUrl);
+        }} variant="secondary" text="Share brain" startIcon={<ShareIcon />}></Button>
+      </div>
+
+      <div className="flex gap-4 flex-wrap">
+  {Array.isArray(content) && content.map(({ type, link, title }, idx) => (
+    <Card key={idx} type={type} link={link} title={title} />
+  ))}
+</div>
+
+    </div>
+  </div>
+}
